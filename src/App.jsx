dee7d8f6cc09a9ac3018e460cdf908fd984dcbfa
asaphoncho <uniqueeph38@gmail.com>
2025-01-05 03:31:27 +0100

    updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,38 @@ import './App.css'
 import data from './data.json'
 
 function App() {
+  let filteredData
   const [roles, setRoles] = useState(data)
+  const [filterTags, setFilterTags] = useState([])
+  if(filterTags > 0){
+    filteredData = data.map(d.role.includes(filterTags))
+    setRoles(filteredData)
+  }
+  function handleTag(event){
+    const tag = event.target.textContent;
+    setFilterTags((f) => [...f, tag]);
+    console.log(filterTags)
+  }
+  function handleClear(){
+    setFilterTags([])
+  }
 
   return (
     <>
       <div className='main-section'>
-        <div className='header'></div>
+        <div className='filter-section'>
+          <div className='added-tags'>
+            {filterTags.map((ft,index) => (
+              <div key={index} className='filtertag'>
+                <div className='tag' style={{borderTopRightRadius:0, borderBottomRightRadius:0}}>{ft}</div>
+                <button className='remove-btn'><i class="fa-solid fa-xmark"></i></button>
+              </div>
+            ))}
+          </div>
+          <button className='clear-btn' onClick={handleClear}>Clear</button>
+        </div>
+        <div className='header'>
+        </div>
         <div className='roles-container'>
           {roles.map((role, index)=>(
             <div key={index} className='role-card' style={role.featured == true ? {borderLeft: 'solid 7px hsl(180, 29%, 50%)'} : null}>
@@ -35,10 +61,10 @@ function App() {
                   </div>
                 </div>
                 <div className='tags'>
-                  <div className='tag'>{role.role}</div>
-                  <div className='tag'>{role.level}</div>
-                  {role.languages.map((language, index) => (<div className='tag' key={index}>{language}</div>))}
-                  {role.tools.map((tool, index) => (<div className='tag' key={index}>{tool}</div>))}
+                  <button className='tag' onClick={handleTag}>{role.role}</button>
+                  <button className='tag' onClick={handleTag}>{role.level}</button>
+                  {role.languages.map((language, index) => (<button className='tag' onClick={handleTag} key={index}>{language}</button>))}
+                  {role.tools.map((tool, index) => (<button className='tag' onClick={handleTag} key={index}>{tool}</button>))}
                 </div>
             </div>
           ))}
